9efb10edc692452cd7f81321d75e918caa3d1c7f
David Castro <davidcastro0796@outlook.es>
2020-07-22 20:04:03 -0500

    autogestion

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,7 +2,7 @@ class ImagesController < ApplicationController
 	before_action :set_image, only:[:show, :edit, :update, :destroy]
 	def index
 		@images = Image.order(id: :desc)
-  					   .where user_id: current_user.id
+		.where user_id: current_user.id
 	end
 
 	def new
@@ -19,32 +19,32 @@ class ImagesController < ApplicationController
 	end
 
 	def show
-    #before_action :set_image, only:[:show, :edit, :update, :destroy]
-  end
+	#before_action :set_image, only:[:show, :edit, :update, :destroy]
+	end
 
 
-  def edit
-    #before_action :set_image, only:[:show, :edit, :update, :destroy]	
-  end
+	def edit
+	#before_action :set_image, only:[:show, :edit, :update, :destroy]	
+	end
 
-  def update
-	  #before_action :set_image, only:[:show, :edit, :update, :destroy]
-	  @image.update image_params
-	  redirect_to @image
+	def update
+	#before_action :set_image, only:[:show, :edit, :update, :destroy]
+	@image.update image_params
+	redirect_to @image
 	end
 
 	def destroy
-    #before_action :set_image, only:[:show, :edit, :update, :destroy]
-    @image.destroy
-    redirect_to images_path
-  end
+	#before_action :set_image, only:[:show, :edit, :update, :destroy]
+	@image.destroy
+	redirect_to images_path
+	end
 
-  private
-		def image_params # son los parametros que vienen desde la vista
-			params.require(:image).permit(:description,:name,:picture)
-		end
+	private
+	def image_params # son los parametros que vienen desde la vista
+		params.require(:image).permit(:description,:name,:picture)
+	end
 
-		def set_image
-			@image = Image.find params[:id]
-		end
+	def set_image
+		@image = Image.find params[:id]
+	end
 	end
